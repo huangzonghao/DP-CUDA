f6aa897ba90052a15d50c9517af1165db0701cce
hzhangaq <hzhangaq@ust.hk>
2015-05-31 21:30:10 +0800

    add a copy for test
    add CCode1

---
+++ CCode1/src/dp_model.h
@@ -0,0 +1,106 @@
+#ifndef DYNAMIC_PROGRAMMING_H
+#define DYNAMIC_PROGRAMMING_H
+
+#include <stdint.h>
+
+// Use 8-bit integer to save space
+// Remember to cast it to 32-bit int
+// when you actually use it!
+#define dp_int int_fast8_t
+
+#ifndef __device__
+#define __device__
+#endif
+
+#ifndef __constant__
+#define __constant__
+#endif
+
+#define MAX_DISTRIBUTION_LENGTH 50
+
+
+extern void init_states(float *);
+extern void iter_states(float *,
+                        dp_int *,
+                        dp_int *,
+                        float *,
+                        int);
+
+
+const unsigned n_period = 2;
+
+const int n_dimension = 4;
+const int n_capacity = 10;
+const float n_drate = 8;
+
+const float unit_salvage = 1.0;
+const float unit_hold = -0.5;
+const float unit_order = -3.0;
+const float unit_price = 5.0;
+const float unit_disposal = -2.0;
+
+const float discount = 0.95;
+
+struct Demand {
+    int min_demand;
+    int max_demand;
+    float distribution[MAX_DISTRIBUTION_LENGTH];
+};
+
+
+__device__ __constant__ struct Demand
+demand_distribution_at_period[n_period] = {
+    // Demand struct for 1st period
+    {
+        0,
+        20,
+        {
+            0.00033546, 0.0026837,  0.0107348,  0.02862614, 0.05725229,
+            0.09160366, 0.12213822, 0.13958653, 0.13958653, 0.12407692,
+            0.09926153, 0.07219021, 0.0481268 , 0.02961649, 0.01692371,
+            0.00902598, 0.00451299, 0.00212376, 0.00094389, 0.00039743
+        }
+    },
+    // Demand struct for 2nd period
+    {
+        0,
+        20,
+        {
+            0.00033546, 0.0026837,  0.0107348,  0.02862614, 0.05725229,
+            0.09160366, 0.12213822, 0.13958653, 0.13958653, 0.12407692,
+            0.09926153, 0.07219021, 0.0481268 , 0.02961649, 0.01692371,
+            0.00902598, 0.00451299, 0.00212376, 0.00094389, 0.00039743
+        }
+    }
+    // and so on
+};
+
+
+/*
+ * Alternatively, if you don't want to supply the same distribution
+ * multiple times, initialize Demand by yourself using macro and supply
+ * it to the array constructor.
+ *
+ * Example:
+
+#define demand1 { \
+    0, \
+    20, \
+    { \
+        0.00033546, 0.0026837,  0.0107348,  0.02862614, 0.05725229, \
+        0.09160366, 0.12213822, 0.13958653, 0.13958653, 0.12407692, \
+        0.09926153, 0.07219021, 0.0481268 , 0.02961649, 0.01692371, \
+        0.00902598, 0.00451299, 0.00212376, 0.00094389, 0.00039743 \
+    } \
+}
+
+__device__ __constant__ struct Demand
+demand_distribution_at_period[n_period] = { demand1, demand1 };
+
+ * Caveat:
+ * Macros can only be one line, so use "\" to escape new line character.
+ * Using variable is more readable, but nvcc has limit on constant variable
+ * initialization, so consider it as an ugly hack.
+ */
+
+#endif /* DYNAMIC_PROGRAMMING_H */
---
+++ CCode1/src/dynamic_programming.cu
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+#include <cuda.h>
+
+// Hack: including source files is bad in general!
+// But we know it is not going to be used anywhere else
+// If you do, write a header file and include it instead!
+#include "dp_model.cu"
+
+
+// Helper function to get CUDA thread id
+// whenever we use __device__ function
+__device__ inline size_t
+get_thread_id() {
+
+    size_t blockId = blockIdx.x +
+                     blockIdx.y * gridDim.x +
+                     gridDim.x * gridDim.y * blockIdx.z;
+    return blockId * blockDim.x + threadIdx.x;
+}
+
+
+// Using these values for general CUDA GPU is just fine
+inline void
+get_grid_dim(dim3* block_dim, dim3* grid_dim, size_t batch_size) {
+
+    size_t n_block = batch_size / 512 + 1;
+
+    assert(block_dim && grid_dim);
+    *block_dim = dim3(512, 1, 1);
+    *grid_dim = dim3(4096, n_block / 4096 + 1, 1);
+}
+
+
+// CUDA Kernel function for initialization
+__global__ void
+init_kernel(float *current_values,
+            size_t d,
+            size_t c,
+            size_t batch_size) {
+
+    size_t idx = get_thread_id();
+
+    if (idx < batch_size) {
+        size_t current = c * batch_size + idx;
+        size_t parent = (c > 0) ? (current - batch_size) : 0;
+
+        current_values[current] = current_values[parent] + 1.0;
+    }
+}
+
+
+// Plain C function for interact with kernel
+void
+init_states(float *current_values) {
+
+    size_t num_states = std::pow(n_capacity, n_dimension);
+
+    // The very first state
+    init_kernel<<<1, 1>>>(current_values,
+                          0, 0, 1);
+
+    for (size_t d = 0; d < n_dimension; d++) {
+        size_t batch_size = pow(n_capacity, d);
+
+        dim3 block_dim, grid_dim;
+        get_grid_dim(&block_dim, &grid_dim, batch_size);
+
+        for (size_t c = 1; c < n_capacity; c++) {
+            init_kernel<<<grid_dim, block_dim>>>(current_values,
+                                                 d, c, batch_size);
+        }
+    }
+
+    cudaDeviceSynchronize();
+    cudaThreadSynchronize();
+
+}
+
+
+// The CUDA kernel function for DP
+__global__ void
+iter_kernel(float *current_values,
+            dp_int *depletion,
+            dp_int *order,
+            float *future_values,
+            int period,
+            size_t d,
+            size_t c,
+            size_t batch_size) {
+
+    size_t idx = get_thread_id();
+
+    if (idx < batch_size) {
+
+        size_t current = c * batch_size + idx;
+        int state[n_dimension+1] = {};
+        decode(state, current);
+        if (period < n_period-1) {
+           int h_depletion = 0;
+           int h_order = 0;
+           if (n_drate - sum(state, n_dimension+1) > 1e-6){
+              h_order = (int) n_drate - sum(state, n_dimension+1);
+           }
+        }
+        else {
+           int h_depletion = 0;
+           for (int i = 1; i < n_dimension + 1; i++) {
+               if (sum(state, i)- i* n_drate > h_depletion + 1e-6){
+                  h_depletion = sum(state, i)- i* n_drate
+               }  
+           }
+           int h_order = 0;
+           if (n_drate - h_depletion - sum(state, n_dimension+1) > 1e-6){
+              h_order = (int) n_drate - h_depletion - sum(state, n_dimension+1);
+           }
+
+        }
+        optimize(current_values,
+                     // the state we are computing
+                  current,
+                     // n_depletion, min_depletion, max_depletion
+                  depletion, h_depletion, h_depletion +1,
+                     // n_order, min_order, max_order
+                  order, h_order, n_order + 1,
+                     // future utility for reference
+                  future_values,
+                  period);
+
+     }
+}
+
+
+// Plain C function to interact with kernel
+// The structure is essentially the same as init_states.
+// If you feel confused, start from there!
+void
+iter_states(float *current_values,
+            dp_int *depletion,
+            dp_int *order,
+            float *future_values,
+            int period) {
+
+    size_t num_states = std::pow(n_capacity, n_dimension);
+
+    // The very first state 0,0,...,0
+    iter_kernel<<<1, 1>>>(current_values,
+                          depletion,
+                          order,
+                          future_values,
+                          period,
+                          0, 0, 1);
+
+    for (size_t d = 0; d < n_dimension; d++) {
+
+        size_t batch_size = pow(n_capacity, d);
+
+        dim3 block_dim, grid_dim;
+        get_grid_dim(&block_dim, &grid_dim, batch_size);
+
+        for (size_t c = 1; c < n_capacity; c++) {
+            iter_kernel<<<grid_dim, block_dim>>>(current_values,
+                                                 depletion,
+                                                 order,
+                                                 future_values,
+                                                 period,
+                                                 d, c, batch_size);
+        }
+    }
+
+    cudaDeviceSynchronize();
+    cudaThreadSynchronize();
+
+}
